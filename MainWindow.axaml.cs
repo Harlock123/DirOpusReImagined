cb9fb9c2c7a47075b39e3e9099b43c06480e86fb
harlo <harlo@WINDEV1612SYS>
2023-05-22 16:24:26 -0400

    Work on Grid Resizing with interface scaling

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -1,12 +1,36 @@
 using Avalonia.Controls;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace DirOpusReImagined
 {
     public partial class MainWindow : Window
     {
+
+        private Avalonia.Rect LRB = new Avalonia.Rect();
+        private Avalonia.Rect RRB = new Avalonia.Rect();
+        private Avalonia.Size OrigSize = new Avalonia.Size();
+
         public MainWindow()
         {
             InitializeComponent();
+
+            MainWindowGridContainer.SizeChanged += MainWindowGridContainer_SizeChanged;
+
+        }
+
+        private void MainWindowGridContainer_SizeChanged(object? sender, SizeChangedEventArgs e)
+        {
+
+            double nwidth = e.NewSize.Width * .45;
+
+            double nheight = e.NewSize.Height * .7;
+
+            LPgrid.SetGridSize((int)nwidth - 8, (int)nheight - 16);
+            RPgrid.SetGridSize((int)nwidth - 8, (int)nheight - 16 );
+
+
+            
+
         }
     }
 }
