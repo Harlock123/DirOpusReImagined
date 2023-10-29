5549d3db443d2e9d799d89819aedcae9777f9a39
Lonnie Watson <harlock123@gmail.com>
2023-10-29 15:44:00 -0400

    Implemented Native Zipfile Creation with Recursive folder adding

--- CreateArchive.axaml.cs
+++ CreateArchive.axaml.cs
@@ -56,17 +56,64 @@ public partial class CreateArchive : Window
         
         while (File.Exists(zipFilePath))
         {
-            
-            string fname = Path.GetFileNameWithoutExtension(zipFilePath);
+            string TempFilePath = Path.Combine(_panelSetup!.PrimaryPath, Zipname.Text);
+            string fname = Path.GetFileNameWithoutExtension(TempFilePath);
             fname = fname + "_" + i.ToString();
-            string ext = Path.GetExtension(zipFilePath);
+            string ext = Path.GetExtension(TempFilePath);
             zipFilePath = Path.Combine(_panelSetup!.PrimaryPath, fname + ext);
-            
+            i += 1;
+
         }
         // zipFilePath should be unique now
 
         var zipArchive = ZipFile.Open(zipFilePath, ZipArchiveMode.Create);
+        
+        foreach (var item in _panelSetup!.PrimaryGrid.SelectedItems)
+        {
+            if (item is AFileEntry fileItem)
+            {
+                if (fileItem.Typ)
+                {
+                    // This particular Item is a Folder
+                    string entryName = Path.GetFileName(fileItem.Name);
+                    string entryPath = Path.Combine(_panelSetup!.PrimaryPath, entryName);
+                }
+                else
+                {
+                    // This particular Item is a File
+                    string entryName = Path.GetFileName(fileItem.Name);
+                    string entryPath = Path.Combine(_panelSetup!.PrimaryPath, entryName);
+                    
+                    zipArchive.CreateEntryFromFile(entryPath, entryName);
+                    
+                    Console.WriteLine($"Added {entryName} to ZIP archive.");
+                }
+                
+            }
+        }
+
+        foreach (var item in _panelSetup!.PrimaryGrid.SelectedItems)
+        {
+            if (item is AFileEntry fileItem)
+            {
+                if (fileItem.Typ)
+                {
+                    // This particular Item is a Folder
+                    string entryName = Path.GetFileName(fileItem.Name);
+                    string entryPath = Path.Combine(_panelSetup!.PrimaryPath, entryName);
+                    
+                    AddFolderToZipRecursive(zipArchive,entryPath ,entryName);
+                    
+                    Console.WriteLine($"Added {entryName} to ZIP archive.");
+                }
+                
+            }
+
+        }
+
         zipArchive.Dispose();
+        
+        
     }
     
     public static void UpdateZipArchive(string zipFilePath, params string[] filesToAdd)
@@ -103,19 +150,30 @@ public partial class CreateArchive : Window
         {
             using (var zipArchive = ZipFile.Open(zipFilePath, ZipArchiveMode.Update))
             {
-                DirectoryInfo directoryInfo = new DirectoryInfo(folderPath);
-                foreach (FileInfo file in directoryInfo.GetFiles())
-                {
-                    string entryName = Path.Combine(directoryInfo.Name, file.Name);
-                    zipArchive.CreateEntryFromFile(file.FullName, entryName);
-                    Console.WriteLine($"Added {entryName} to ZIP archive.");
-                }
+                AddFolderToZipRecursive(zipArchive, folderPath, string.Empty);
             }
-            Console.WriteLine("Folder added to the ZIP archive successfully.");
+            Console.WriteLine("Folder and its contents added to the ZIP archive successfully.");
         }
         catch (Exception ex)
         {
             Console.WriteLine($"Error adding folder to the ZIP archive: {ex.Message}");
+        }
+    }
+    
+    private static void AddFolderToZipRecursive(ZipArchive zipArchive, string folderPath, string relativePath)
+    {
+        foreach (var file in Directory.GetFiles(folderPath))
+        {
+            string entryName = Path.Combine(relativePath, Path.GetFileName(file));
+            zipArchive.CreateEntryFromFile(file, entryName);
+            Console.WriteLine($"Added {entryName} to ZIP archive.");
+        }
+
+        foreach (var subdirectory in Directory.GetDirectories(folderPath))
+        {
+            string subdirectoryName = Path.GetFileName(subdirectory);
+            string subdirectoryRelativePath = Path.Combine(relativePath, subdirectoryName);
+            AddFolderToZipRecursive(zipArchive, subdirectory, subdirectoryRelativePath);
         }
     }
 
